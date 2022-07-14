db89d71eb5350c72e341773f28acc61953896805
Deloxo <37124397+Deloxo@users.noreply.github.com>
2022-07-13 21:20:54 -0400

    did a lot of stuff this time

--- pageTransitions.js
+++ pageTransitions.js
@@ -1,75 +1,91 @@
-function mainPage() {
-    document.getElementById("settingsPage").style.display = 'none';
-    document.getElementById("charactersPage").style.display = 'none';
-    document.getElementById("spellListTab").style.border = '3px #760f78 solid';
-    document.getElementById("settingsTab").style.border = 'none';
-    document.getElementById("charactersTab").style.border = 'none';
+function searchResults() {
+ for (var x = 0; x < Object.keys(JSON.parse(localStorage.spells)).length; x++) {
+   document.getElementsByClassName("item")[x].style.display = '';
+ }
+ for (var x = 0; x < Object.keys(JSON.parse(localStorage.spells)).length; x++) {
+     if (Object.values(JSON.parse(localStorage.spells))[x].name.includes(document.getElementById("searchBar").value) == false) {
+       document.getElementsByClassName("item")[x].style.display = 'none';
+     }
+ }
 }
 
-function settingsPage() {
-    document.getElementById("settingsPage").style.display = 'initial';
-    document.getElementById("charactersPage").style.display = 'none';
-    document.getElementById("settingsTab").style.border = '3px #760f78 solid';
-    document.getElementById("spellListTab").style.border = 'none';
-    document.getElementById("charactersTab").style.border = 'none';
-
+function backToList() {
+  document.getElementById("itemPage").style.display = 'none';
+  document.getElementById("filtersPage").style.display = 'none';
+  document.getElementById("createPage").style.display = 'none';
+  document.getElementById("editPage").style.display = 'none';
 }
 
-function charactersPage() {
-    document.getElementById("settingsPage").style.display = 'none';
-    document.getElementById("charactersPage").style.display = 'initial';
-    document.getElementById("charactersTab").style.border = '3px #760f78 solid';
-    document.getElementById("settingsTab").style.border = 'none';
-    document.getElementById("spellListTab").style.border = 'none';
+function openListsTab() {
+    document.getElementById("listsTab").style.display = 'none';
+    document.getElementById("charactersTab").style.display = 'none';
+    document.getElementById("settingsTab").style.display = 'none';
+    document.getElementById("listsTabButton").style.border = '3px #760f78 solid';
+    document.getElementById("charactersTabButton").style.border = 'none';
+    document.getElementById("settingsTabButton").style.border = 'none';
 }
 
-function editSpell() {
-    document.getElementById("spellPage").style.display = 'none';
-    document.getElementById("spellEdit").style.display = 'initial';
+function openCharactersTab() {
+    document.getElementById("listsTab").style.display = 'none';
+    document.getElementById("charactersTab").style.display = 'initial';
+    document.getElementById("settingsTab").style.display = 'none';
+    document.getElementById("listsTabButton").style.border = 'none';
+    document.getElementById("charactersTabButton").style.border = '3px #760f78 solid';
+    document.getElementById("settingsTabButton").style.border = 'none';
+}
+
+function openSettingsTab() {
+    document.getElementById("listsTab").style.display = 'none';
+    document.getElementById("charactersTab").style.display = 'none';
+    document.getElementById("settingsTab").style.display = 'initial';
+    document.getElementById("listsTabButton").style.border = 'none';
+    document.getElementById("charactersTabButton").style.border = 'none';
+    document.getElementById("settingsTabButton").style.border = '3px #760f78 solid';
+}
+
+function openCreatePage() {
+    document.getElementById("createPage").style.display = 'initial';
+}
+
+function cancelCreation() {
+    document.getElementById("createPage").style.display = 'none';
+}
+
+function openFiltersPage() {
+  document.getElementById("filtersPage").style.display = 'initial';
+}
+
+function openItemPage(number) {
+    itemIndex = number;
+    document.getElementById("itemPageTitle").innerHTML = Object.values(spells)[number].name;
+    document.getElementById("itemPage").style.display = 'initial';
+}
+
+function editItem() {
+    document.getElementById("itemPage").style.display = 'none';
+    document.getElementById("editPage").style.display = 'initial';
     document.getElementById("nameTextBoxEdit").value = Object.values(spells)[spellIndex].name;
 }
 
 function cancelEdit() {
-    document.getElementById("spellPage").style.display = 'initial';
-    document.getElementById("spellEdit").style.display = 'none';
+    document.getElementById("itemPage").style.display = 'initial';
+    document.getElementById("editPage").style.display = 'none';
 }
 
-function createSpell() {
-    document.getElementById("spellPage").style.display = 'none';
-    document.getElementById("spellCreate").style.display = 'initial';
+function openSpellsTab() {
+  document.getElementById("spellsTab").style.display = 'initial';
+  document.getElementById("traitsTab").style.display = 'none';
+  document.getElementById("rulesTab").style.display = 'none';
 }
 
-function cancelCreation() {
-    document.getElementById("spellCreate").style.display = 'none';
+function openTraitsTab() {
+  document.getElementById("spellsTab").style.display = 'none';
+  document.getElementById("traitsTab").style.display = 'initial';
+  document.getElementById("rulesTab").style.display = 'none';
 }
 
-function spellPage(number) {
-    spellIndex = number;
-    document.getElementById("spellPageTitle").innerHTML = Object.values(spells)[number].name;
-    document.getElementById("spellPage").style.display = 'initial';
-}
-
-function spellsTab() {
-  document.getElementById("skillsTabPage").style.display = 'none';
-  document.getElementById("rulesTabPage").style.display = 'none';
-}
-
-function skillsTab() {
-  document.getElementById("skillsTabPage").style.display = '';
-  document.getElementById("rulesTabPage").style.display = 'none';
-}
-
-function rulesTab() {
-  document.getElementById("skillsTabPage").style.display = 'none';
-  document.getElementById("rulesTabPage").style.display = '';
+function openRulesTab() {
+  document.getElementById("spellsTab").style.display = 'none';
+  document.getElementById("traitsTab").style.display = 'none';
+  document.getElementById("rulesTab").style.display = 'initial';
  }
-function searchResultsChange() {
-  for (var x = 0; x < Object.keys(JSON.parse(localStorage.spells)).length; x++) {
-    document.getElementsByClassName("spell")[x].style.display = '';
-  }
-  for (var x = 0; x < Object.keys(JSON.parse(localStorage.spells)).length; x++) {
-      if (Object.values(JSON.parse(localStorage.spells))[x].name.includes(document.getElementById("searchBar").value) == false) {
-        document.getElementsByClassName("spell")[x].style.display = 'none';
-      }
-  }
-}
